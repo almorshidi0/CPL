a13c6ada30e6abeb91a45d6876d5acc1ad6777b1
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-08-19 21:52:53 +0300

    CPL - CH 1 - SEC 1.3

---
+++ ch_1/ex_1_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* print Fahrenheit−Celsius table
+    for fahr = 0, 20, ..., 300; floating−point version
+    Reversed Order */
+main()
+{
+    int fahr;
+    
+    printf("Fahr\tCelsius\n");
+    for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+    {
+        printf("%4d\t%7d\n", fahr, 5 * (fahr - 32) / 9);
+    }
+}
