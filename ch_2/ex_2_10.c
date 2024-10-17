3343fa09da00f599c740c5d4e8080bd57624a0ef
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-10-17 20:05:00 +0300

    CPL - CH 2 - SEC 2.11 & SEC 2.12

---
+++ ch_2/ex_2_10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char lower(char c);
+
+int main(){
+    printf("%c\n", lower('c'));
+
+    return 0;
+}
+
+char lower(char c){
+    (c >= 'A' && c <= 'Z') ? c += 'a' - 'A' : c;
+
+    return c;
+} 
