b6de0b6c86d726441e29f4ceff611f125d1971d6
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-10-19 15:12:47 +0300

    CPL - CH 3 - SEC 3.1 & SEC 3.2 & SEC 3.3

---
+++ ch_3/ex_3_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n);
+
+int main(){
+    int arr[] = {2, 4, 6, 7, 9, 29, 45, 46, 49, 50, 51};
+
+    printf("%d\n", binsearch(9, arr, 10));
+
+    return 0;
+}
+
+int binsearch(int x, int v[], int n){
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high && x != v[mid]){
+        mid = (low+high) / 2;
+        if(x < v[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    if(x != v[mid]) /* no match */
+        mid = -1;
+
+    return mid;
+}
