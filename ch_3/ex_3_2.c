7d3b74cec137c90d37b26d35b5b2366c57016bc3
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-10-19 21:16:56 +0300

    CPL - CH 3 - SEC 3.4

---
+++ ch_3/ex_3_2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+void escape(char to[], char from[]);
+void unescape(char to[], char from[]);
+
+int main(){
+    int len;
+    char line[MAXLINE]  = "Hello,\tworld!\n";
+    char escaped[MAXLINE];
+    char unescaped[MAXLINE];
+
+    printf("%s\n", line);
+    escape(escaped, line);
+    printf("%s\n", escaped);
+    unescape(unescaped, escaped);
+    printf("%s\n", unescaped);
+    
+    return 0;
+}
+
+void escape(char to[], char from[]){
+    int i = 0, j = 0;
+
+    while((to[j] = from[i]) != '\0'){
+        switch(from[i]){
+            case '\t':
+                to[j] = '\\';
+                ++j;
+                to[j] = 't';
+                break;
+            case '\n':
+                to[j] = '\\';
+                ++j;
+                to[j] = 'n';
+                break;
+        }
+        ++i;
+        ++j;
+    }
+}
+
+void unescape(char to[], char from[]){
+    int i = 0, j = 0;
+
+    while((to[j] = from[i]) != '\0'){
+        if(from[i] == '\\'){
+            switch(from[i + 1]){
+                case 't':
+                    to[j] = '\t';
+                    ++i;
+                    break;
+                case 'n':
+                    to[j] = '\n';
+                    ++i;
+                    break;
+            }
+        }
+        ++i;
+        ++j;
+    }
+}
