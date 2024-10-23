1e4a796331e5b12362f2d9622a73f54b46896395
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-10-23 13:21:00 +0300

    CPL - CH 3 - SEC 3.6 & SEC 3.7 & SEC 3.8

---
+++ ch_3/ex_3_4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ABS(number)  (((number) < 0)? -(number) : (number))
+
+void reverse(char s[]);
+void itoa(int n, char s[]);
+
+int main(){
+    int num = 2147483647; // Max value for a 32-bit signed integer
+    char result[20];
+    itoa(num, result);
+    printf("%d -> %s\n", num, result);
+
+    num = -2147483648; // Min value for a 32-bit signed integer
+    itoa(num, result);
+    printf("%d -> %s\n", num, result);
+    
+    return 0;
+}
+
+/* itoa: convert n to characters in s */
+void itoa(int n, char s[]){
+    int i, sign;
+
+    sign = n;
+    i = 0;
+
+    do { /* generate digits in reverse order */
+            s[i++] = ABS(n % 10) + '0'; /* get next digit */
+    } while ((n /= 10) != 0); /* delete it */
+
+    if (sign < 0)
+        s[i++] = '-';
+    
+    s[i] = '\0';
+    reverse(s);
+}
+
+/* reverse: reverse string s in place */
+void reverse(char s[]){
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--){
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
